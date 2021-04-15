aef1c77df2bd4cee7e839c4cb17c957c360f59a3
Achyutha-Kalal <kalalachyuthagoud@gmail.com>
2021-04-16 00:36:25 +0530

    Code Format and pre order tree construction

--- Trees/BinarySearchTree.java
+++ Trees/BinarySearchTree.java
@@ -1,96 +1,114 @@
+
+/*
+Developed by Achyutha Kalal
+Contains the BinarySearchTree structure with basic operations
+*/
 import java.util.LinkedList;
 import java.util.Queue;
 
+//Structure of BinarySearch Tree
 public class BinarySearchTree {
     private BinaryNode root;
-    private BinaryNode insertRecursively(BinaryNode curr,int val)
-{
-    if(curr==null){
-        return (new BinaryNode(val));
-    }
-    if(curr.data>val)
-    {
-        curr.left=insertRecursively(curr.left, val);
-    }
-    if(curr.data<val)
-    {
-        curr.right=insertRecursively(curr.right, val);
-    }
-    if(curr.data==val)
-    {
-        System.out.println("Duplicate values are not allowed");
+
+    // insertion method
+    private BinaryNode insertRecursively(BinaryNode curr, int val) {
+        if (curr == null) {
+            return (new BinaryNode(val));
+        }
+        if (curr.data > val) {
+            curr.left = insertRecursively(curr.left, val);
+        }
+        if (curr.data < val) {
+            curr.right = insertRecursively(curr.right, val);
+        }
+        if (curr.data == val) {
+            System.out.println("Duplicate values are not allowed");
+            return curr;
+        }
         return curr;
     }
-    return curr;
-}    
-private void inOrderTraversal(BinaryNode node)
-{
-    if(node!=null)
-    {
-        inOrderTraversal(node.left);
-        System.out.print(node.data+" ");
-        inOrderTraversal(node.right);
-    }
-}
-private void preOrderTraversal(BinaryNode node)
-{
-    if(node!=null)
-    {
-        System.out.print(node.data+" ");
-        preOrderTraversal(node.left);
-        preOrderTraversal(node.right);
-    }
-}
-private void postOrderTraversal(BinaryNode node)
-{
-    if(node!=null)
-    {
-        postOrderTraversal(node.left);
-        postOrderTraversal(node.right);
-        System.out.print(node.data+" ");
-    }
-}
-private void levelOrderTraversal(BinaryNode node)
-{
-    Queue<BinaryNode> nodeQueue=new LinkedList<>();
-    nodeQueue.add(node);
-    while(!nodeQueue.isEmpty())
-    {
-        BinaryNode temp=nodeQueue.poll();
-        System.out.print(temp.data+" ");
-        if(temp.left!=null)
-        nodeQueue.add(temp.left);
-        if(temp.right!=null)
-        nodeQueue.add(temp.right);
 
+    // In order traversal method
+    private void inOrderTraversal(BinaryNode node) {
+        if (node != null) {
+            inOrderTraversal(node.left);
+            System.out.print(node.data + " ");
+            inOrderTraversal(node.right);
+        }
     }
-}
-public void insertNode(int val)
-{
-    this.root=insertRecursively(this.root, val);
-}
-public void printInOrder()
-{
-    System.out.println("In Order Traversal");
-    inOrderTraversal(this.root);
-    System.out.println();
-}
-public void printPreOrder()
-{
-    System.out.println("Pre Order Traversal");
-    preOrderTraversal(this.root);
-    System.out.println();
-}
-public void printPostOrder()
-{
-    System.out.println("Post Order Traversal");
-    postOrderTraversal(this.root);
-    System.out.println();
-}
-public void printLevelOrder()
-{
-    System.out.println("Level Order Traversal");
-    levelOrderTraversal(this.root);
-    System.out.println();
-}
+
+    // Pre Order traversal method
+    private void preOrderTraversal(BinaryNode node) {
+        if (node != null) {
+            System.out.print(node.data + " ");
+            preOrderTraversal(node.left);
+            preOrderTraversal(node.right);
+        }
+    }
+
+    // Post Order traversal method
+    private void postOrderTraversal(BinaryNode node) {
+        if (node != null) {
+            postOrderTraversal(node.left);
+            postOrderTraversal(node.right);
+            System.out.print(node.data + " ");
+        }
+    }
+
+    // Level Order traversal method
+    private void levelOrderTraversal(BinaryNode node) {
+        Queue<BinaryNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(node);
+        while (!nodeQueue.isEmpty()) {
+            BinaryNode temp = nodeQueue.poll();
+            System.out.print(temp.data + " ");
+            if (temp.left != null)
+                nodeQueue.add(temp.left);
+            if (temp.right != null)
+                nodeQueue.add(temp.right);
+
+        }
+    }
+
+    // Root Node insertion method
+    public void insertNode(int val) {
+        this.root = insertRecursively(this.root, val);
+    }
+
+    // Root Node InOrder traversal print method
+    public void printInOrder() {
+        System.out.println("In Order Traversal");
+        inOrderTraversal(this.root);
+        System.out.println();
+    }
+
+    // Root Node PreOrder traversal print method
+    public void printPreOrder() {
+        System.out.println("Pre Order Traversal");
+        preOrderTraversal(this.root);
+        System.out.println();
+    }
+
+    // Root Node PostOrder traversal print method
+    public void printPostOrder() {
+        System.out.println("Post Order Traversal");
+        postOrderTraversal(this.root);
+        System.out.println();
+    }
+
+    // Root Node LevelOrder traversal print method
+    public void printLevelOrder() {
+        System.out.println("Level Order Traversal");
+        levelOrderTraversal(this.root);
+        System.out.println();
+    }
+
+    // constructing rootNode from if preOrder is given
+    public void constructFromPreOrder(int arr[]) {
+        BinaryNode curr = new BinaryNode(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            curr = insertRecursively(curr, arr[i]);
+        }
+        this.root = curr;
+    }
 }
--- Trees/TreesImplementor.java
+++ Trees/TreesImplementor.java
@@ -2,16 +2,8 @@ public class TreesImplementor {
     public static void main(String args[])
     {
         BinarySearchTree BST=new BinarySearchTree();
-        BST.insertNode(5);
-        BST.insertNode(3);
-        BST.insertNode(4);
-        BST.insertNode(2);
-        BST.insertNode(7);
-        BST.insertNode(8);
-        BST.insertNode(6);
-        BST.printInOrder();
-        BST.printPreOrder();
-        BST.printPostOrder();
+        int arr[]={10, 5, 1, 7, 40, 50};
+        BST.constructFromPreOrder(arr);
         BST.printLevelOrder();
     }
     
