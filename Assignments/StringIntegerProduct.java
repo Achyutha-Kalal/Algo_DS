41ce39ca65db4848ca651e12f55a09c29ea0c140
Achyutha-Kalal <kalalachyuthagoud@gmail.com>
2021-05-25 16:43:28 +0530

    Add another string product integers

---
+++ Assignments/StringIntegerProduct.java
@@ -0,0 +1,16 @@
+public class StringIntegerProduct {
+    public static void main(String[] args) {
+        
+        int arr[]={0,1,2,3,4,5,6,7,8,9};
+        String s1="123";
+        String s2="100";
+        int sum1=0,sum2=0;
+        int l1=s1.length();
+        int l2=s2.length();
+        for(int i=l1-1;i>=0;i++)
+        {
+            s1.charAt(i);
+        }
+    }
+    
+}
