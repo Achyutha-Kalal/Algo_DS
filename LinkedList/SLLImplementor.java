0b2cd4d223ee524807969998b50ea430ccb715f7
Achyutha-Kalal <kalalachyuthagoud@gmail.com>
2021-06-12 10:14:19 +0530

    LinkedList classes addition
    Adding the linkedlist basic structure and insert operations classes

---
+++ LinkedList/SLLImplementor.java
@@ -0,0 +1,10 @@
+public class SLLImplementor {
+    public static void main(String args[])
+{
+    SingleLinkedList head=new SingleLinkedList();
+    head.insertLast(5);
+    head.insertLast(6);
+    head.printList();
+}
+    
+}
